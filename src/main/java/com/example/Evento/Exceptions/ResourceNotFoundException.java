023336dcdd4831529ab2d29c3553ac21b9af0452
William S <will107673@gmail.com>
2026-09-15 22:52:10 -0500

    ResoruceNotFoundException

--- src/main/java/com/example/Evento/Exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/Evento/Exceptions/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.Evento.Exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
