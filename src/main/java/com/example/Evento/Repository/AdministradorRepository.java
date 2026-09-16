e2d69244493a216160400597d3794adb5af422a5
Julian Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2026-09-15 22:25:05 -0500

    Creacion Administrador Repository

--- src/main/java/com/example/Evento/Repository/AdministradorRepository.java
+++ src/main/java/com/example/Evento/Repository/AdministradorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdministradoRepository extends JpaRepository<Administrador, Long> {
+public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
 }
