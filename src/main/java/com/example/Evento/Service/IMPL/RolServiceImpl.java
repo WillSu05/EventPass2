bfd72392fd3d3fa2cf18df2e6d9f25326ef6254a
William S <will107673@gmail.com>
2026-09-17 00:13:59 -0500

    Correccion RolServiceImpl

--- src/main/java/com/example/Evento/Service/IMPL/RolServiceImpl.java
+++ src/main/java/com/example/Evento/Service/IMPL/RolServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class RolServiceImpl implements RolService {
 
     private final RolRepository rolRepository;
