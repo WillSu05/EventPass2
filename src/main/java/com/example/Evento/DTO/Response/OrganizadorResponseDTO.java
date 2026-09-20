26817708c3c0892a0e2eb1e1dcd33cd55db4e72b
Julian Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2026-09-20 18:53:01 -0500

    Organizador DTO

---
+++ src/main/java/com/example/Evento/DTO/Response/OrganizadorResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.Evento.DTO.Response;
+
+import lombok.Data;
+
+@Data
+public class OrganizadorResponseDTO {
+    private Long id;
+    private String nombre;
+    private String email;
+}
