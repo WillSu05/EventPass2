ed13851b34bd3e0b99837dd7a409b39abefb6d23
Julian Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2026-09-20 18:05:14 -0500

    Asistente DTO

---
+++ src/main/java/com/example/Evento/DTO/Request/AsistenteRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.Evento.DTO.Request;
+
+import lombok.Data;
+
+@Data
+public class AsistenteRequestDTO {
+    private String nombre;
+    private String email;
+    private String password;
+    private String telefono;
+}
---
+++ src/main/java/com/example/Evento/DTO/Response/AsistenteResponseDTO.java
@@ -0,0 +1,11 @@
+package com.example.Evento.DTO.Response;
+
+import lombok.Data;
+
+@Data
+public class AsistenteResponseDTO {
+    private Long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+}
