7fad1a6625b53b0fdd6da2934116fdcfba86d342
Julian Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2026-09-20 15:16:09 -0500

    Validacion Repository

--- src/main/java/com/example/Evento/Repository/ValidacionRepository.java
+++ src/main/java/com/example/Evento/Repository/ValidacionRepository.java
@@ -1,9 +1,9 @@
 package com.example.Evento.Repository;
 
-import com.example.Evento.Entity.EstadoOrden;
+import com.example.Evento.Entity.Validacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstadoOrdenRepository extends JpaRepository<EstadoOrden, Long> {
+public interface ValidacionRepository extends JpaRepository<Validacion, Long> {
 }
