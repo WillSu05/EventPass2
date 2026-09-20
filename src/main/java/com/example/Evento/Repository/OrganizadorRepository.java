c329a9334d86cd90b1b658c682488ed412d228bd
Julian Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2026-09-20 18:47:57 -0500

    Organizador Repository

--- src/main/java/com/example/Evento/Repository/OrganizadorRepository.java
+++ src/main/java/com/example/Evento/Repository/OrganizadorRepository.java
@@ -2,6 +2,12 @@ package com.example.Evento.Repository;
 
 import com.example.Evento.Entity.Extends.Organizador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface OrganizadorRepository extends JpaRepository<Organizador, Long> {
+    Optional<Organizador> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
